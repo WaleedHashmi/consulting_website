40119a0a2845e42d4173f32eca4f85e68336b577
Abdullah Muhammad Athar <ama86@cantab.ac.uk>
2024-05-12 13:48:08 +0100

    chore: psql setup

---
+++ backend/database/__init__.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DB_USER = 'postgres'
+DB_PASSWORD = ''
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = 'blog_database'
+
+DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+engine = create_engine(DATABASE_URI)
+
+Session = sessionmaker(bind=engine)
---
+++ backend/database/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class BlogPost(Base):
+    __tablename__ = 'blog_posts'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    description = Column(Text, nullable=False)
+    content = Column(Text, nullable=False)
