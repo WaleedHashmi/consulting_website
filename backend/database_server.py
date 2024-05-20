012f4c72d538df3e5c8c11c0c0bf0d5b63f02358
Abdullah Muhammad Athar <ama86@cantab.ac.uk>
2024-05-20 15:49:53 +0100

    feat: dockerise services

--- backend/database_server.py
+++ backend/database_server.py
@@ -1,4 +1,5 @@
 # main.py
+import os
 from typing import List
 
 import psycopg2
@@ -8,9 +9,20 @@ from pydantic import BaseModel
 
 BLOGS_TABLE = "blog_posts"
 # TODO: secrets file
-conn = psycopg2.connect(
-    dbname="blogs_database", user="postgres", password="", host="localhost"
-)
+
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT"),
+    )
+    return conn
+
+
+conn = get_db_connection()
 
 app = FastAPI()
 
